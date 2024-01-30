89a49f949c76bdb0df6116bfb963fc864d92cdc9
MoulOut <mmoulinlima@outlook.com>
2024-01-30 03:32:27 -0300

    update: usuarios

--- api/services/usuarioService.js
+++ api/services/usuarioService.js
@@ -27,6 +27,48 @@ class UsuarioService {
       throw new Error('Erro ao cadastrar usuario.');
     }
   }
+
+  async buscarTodosUsuarios() {
+    const usuarios = await database.usuarios.findAll();
+    return usuarios;
+  }
+  
+  async buscarUsuarioPorId(id) {
+    const usuario = await database.usuarios.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!usuario) {
+      throw new Error('Usuario informado não cadastrado!');
+    }
+    return usuario;
+  }
+
+  async editarUsuario(dto) {
+    const usuario = await this.buscarUsuarioPorId(dto.id);
+    try {
+      usuario.nome = dto.nome;
+      usuario.email = dto.email;
+      await usuario.save();
+      return usuario;
+    } catch (error) {
+      throw new Error('Erro ao editar usuario!');
+    }
+  }
+
+  async deletarUsuario(id) {
+    await this.buscarUsuarioPorId(id);
+    try {
+      await database.usuarios.destroy({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      throw new Error('Erro ao tentar deletar o usuario!');
+    }
+  }
 }
 
 module.exports = UsuarioService;
