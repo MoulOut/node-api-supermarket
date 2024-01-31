5fabfea70a75f5b302e5a12ae25aa134621b7f55
MoulOut <mmoulinlima@outlook.com>
2024-01-31 04:32:23 -0300

    add: relationship on sequelize models

--- api/models/permissoes.js
+++ api/models/permissoes.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+const usuarios = require('./usuarios');
 module.exports = (sequelize, DataTypes) => {
   class permissoes extends Model {
     /**
@@ -10,15 +9,27 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      permissoes.belongsToMany(models.roles, {
+        through: models.roles_permissoes,
+        as: 'permissa_role',
+        foreignKey: 'permissao_id',
+      });
+      permissoes.belongsToMany(models.usuarios, {
+        through: models.usuarios_permissoes,
+        as: 'permissao_usuario',
+        foreignKey: 'permissao_id',
+      });
     }
   }
-  permissoes.init({
-    nome: DataTypes.STRING,
-    descricao: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'permissoes',
-  });
+  permissoes.init(
+    {
+      nome: DataTypes.STRING,
+      descricao: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'permissoes',
+    }
+  );
   return permissoes;
 };
