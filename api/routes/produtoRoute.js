982c61e54b0c4448072fcb65f5e571d501829ff9
MoulOut <mmoulinlima@outlook.com>
2024-02-01 03:34:46 -0300

    feat: middleware de roles

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,16 +1,14 @@
-const { Router } = require('express')
-const ProdutoController = require('../controllers/produtoController')
-const autenticado = require('../middleware/autenticado')
+const { Router } = require('express');
+const ProdutoController = require('../controllers/produtoController');
+const roles = require('../middleware/roles');
 
-const router = Router()
-
-router.use(autenticado)
+const router = Router();
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', ProdutoController.buscarTodosProdutos)
+  .get('/produto', roles(['Vendedor','Gerente']), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
-  .put('/produto/id/:id', ProdutoController.editarProduto)
+  .put('/produto/id/:id', ProdutoController.editarProduto);
 
-module.exports = router
+module.exports = router;
